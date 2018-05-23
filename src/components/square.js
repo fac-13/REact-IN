84dc206535d19cbc3c34fd9f8ae5411ea2fbf1b4
Nicos <nick.zonias@gmail.com>
2018-05-23 15:49:17 +0100

    create build component Relates to #5

--- src/components/square.js
+++ src/components/square.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 class Square extends React.Component {
 render() {
   return (
-  <button className="square">Hello
+  <button className="square">
+  {this.props.value}
   </button>
     )
   } 
