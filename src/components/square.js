bb120fb2f350d5d590ecf784dcb475e631278995
Nicos <nick.zonias@gmail.com>
2018-05-23 16:03:21 +0100

    test board component Relates to #6

--- src/components/square.js
+++ src/components/square.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class Square extends React.Component {
 render() {
   return (
-  <button className="square">
+  <button className="square" value={this.props.value}>
   {this.props.value}
   </button>
     )
