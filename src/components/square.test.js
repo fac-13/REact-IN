b00d3cc05cf64dcc94cfe15d9807b478f8b62062
isnotafunction <ivana.cosgrove@gmail.com>
2018-05-23 15:31:47 +0100

    adds test for Square component

---
+++ src/components/square.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-dom/test-utils'
+import Square from './square'
+import { render, Simulate, prettyDOM } from 'react-testing-library';
+
+describe('testing square component', () => {
+    test('button renders', () => {
+        const root = document.createElement('div');
+        ReactDOM.render(<Square />, root);    
+        expect(root.querySelector('button')).toBeTruthy()
+  })
+
+})
