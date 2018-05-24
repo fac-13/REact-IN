9f3341427cf4db6fdac7b00f09953764fa57f269
isnotafunction <ivana.cosgrove@gmail.com>
2018-05-24 10:57:49 +0100

    add handleClick function relates #14

--- src/components/board.js
+++ src/components/board.js
@@ -5,10 +5,16 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    squares: Array(25).fill(null)
+      squares: Array(25).fill(null),  
    }
   }
   
+  handleClick = (i) => {
+    const clonedSquares = JSON.parse(JSON.stringify(this.state.squares))
+    clonedSquares[i] = "💩";
+    this.setState({squares: clonedSquares})
+  }
+
   renderSquare(i) {
     return (
       <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>
