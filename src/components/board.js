d20929ed5a51e76e6a648720aa43c9f4ca8670c8
Nicos <nick.zonias@gmail.com>
2018-05-25 06:41:20 +0100

    add components for players and include to the main page

--- src/components/board.js
+++ src/components/board.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Square from './square';
 import checkWinner from '../utils/checkWinner';
-import { fetchData, API_BASE } from "../utils/fetchAvatar";
-
 
 
 class Board extends React.Component {
@@ -11,22 +9,13 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(25).fill(null),
-      xIsNext: true,
-      data: null
+      xIsNext: true
     }
   }
-
-  componentDidMount() {
-    const username = "nico";
-    const url = `${API_BASE}/${username}.png`;
-    fetchData(url).then(data => this.setState({ data }));
-  }
-
 
   handleClick = (i) => {
     const clonedSquares = JSON.parse(JSON.stringify(this.state.squares))
     if (checkWinner(clonedSquares) || clonedSquares[i]){
-      console.log(checkWinner)
       return;
     }
     clonedSquares[i] =  this.state.xIsNext ? "X" : "O";
@@ -44,14 +33,6 @@ class Board extends React.Component {
     
     render() {
 
-      if (!this.state.data) {
-        return <h3>...Loading</h3>;
-      }
-  
-      const avatar = this.state.data.url;
-      const image =  <img src={this.state.data.url} />;
-
-      console.log(avatar);
 
       const winner = checkWinner(this.state.squares);
       let status;
